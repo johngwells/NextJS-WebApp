93fef7c9ad3fb08033dcd5cc1ec65961feab069d
John Wells <molekewl2017@gmail.com>
2022-01-20 22:37:35 -0800

    add google font, custom document  & css

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang='en'>
+        <Head>
+          <link
+            rel='preload'
+            href='/fonts/Lora-Italic.ttf'
+            as='font'
+            crossOrigin='anonymous'
+          ></link>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;

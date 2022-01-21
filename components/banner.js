ed8c1d2d8dc366c20806e18fcbac97c3bc660aea
John Wells <molekewl2017@gmail.com>
2022-01-21 02:35:10 -0800

    complete css for hero section

--- components/banner.js
+++ components/banner.js
@@ -5,7 +5,7 @@ const Banner = props => {
     <div className={styles.container}>
       <h1 className={styles.title}>
         <span className={styles.title1}>Coffee</span>
-        <span className={styles.title2}>Connoisseur</span>
+        <span className={styles.title2}>& Cream</span>
       </h1>
       <p className={styles.subTitle}>Discover local coffee shops!</p>
       <button className={styles.button} onClick={props.onClick}>
